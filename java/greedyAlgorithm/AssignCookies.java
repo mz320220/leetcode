b96ad8340cacd3059b18ebafdd161d49b3477f89
ericma <martin320220@gmail.com>
2022-01-12 11:18:17 +0800

    455AssignCookies

---
+++ java/greedyAlgorithm/AssignCookies.java
@@ -0,0 +1,25 @@
+package greedyAlgorithm;
+
+import java.util.Arrays;
+
+/**
+ * 455. 分发饼干
+ */
+public class AssignCookies {
+
+    public static void main(String[] args) {
+        int[] g = {1, 2, 3}; //胃口值
+        int[] s = {1, 3}; //饼干大小
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int child = 0;
+        int cookie = 0;
+        while(child < g.length && cookie < s.length){
+            if (g[child] <= s[cookie]){
+                child++;
+            }
+            cookie++;
+        }
+        System.out.println(child);
+    }
+}
